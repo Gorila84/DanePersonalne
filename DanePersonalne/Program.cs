cbb272b915d1141a612e4c8c0385d884dbd6ccdf
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-21 12:18:10 +0100

    Add project files.

---
+++ DanePersonalne/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string name;
+string surname;
+int phoneNumber;
+string email;
+decimal growth;
+decimal weight;
+
+Console.WriteLine("Podaj imię: ");
+name = Console.ReadLine();
+
+Console.WriteLine("Podaj nazwisko: ");
+surname = Console.ReadLine();
+
+Console.WriteLine("Podaj numer telefonu: ");
+Int32.TryParse(Console.ReadLine(), out phoneNumber);
+
+Console.WriteLine("Podaj numer email: ");
+email = Console.ReadLine();
+
+Console.WriteLine("Podaj wzrost: ");
+decimal.TryParse(Console.ReadLine(), out growth);
+
+Console.WriteLine("Podaj wagę: ");
+decimal.TryParse(Console.ReadLine(), out weight);
+
+Console.WriteLine($"Twoje dane: \n Imię: {name} \n " +
+                                 $"Nazwisko: {surname} \n Numer telefonu: {phoneNumber} \n" +
+                                 $"Email: {email} \n Wzrost: {growth} \n Waga: {weight}");
